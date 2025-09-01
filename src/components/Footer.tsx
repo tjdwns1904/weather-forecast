fa54b077433aefe57921d954c6bbdf77530f8cd5
tjdwns99 <beans2853@gmail.com>
2025-09-01 11:20:30 +0900

    [text] footer text edit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ function Footer() {
                 <a className="insta icon" title="Visit my Instagram" href={INSTA_LINK} target="_blank"></a>
             </div>
             <div className="line"></div>
-            <p>@copyright: 2025 SJH All Rights Reserved.</p>
+            <p>© 2025 SEONGJOON HONG. All Rights Reserved.</p>
         </div>
     )
 }
