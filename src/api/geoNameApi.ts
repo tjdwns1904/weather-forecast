1b62d6e37ea5755793786945b397a3d32a02abad
tjdwns99 <beans2853@gmail.com>
2025-10-12 18:22:03 +0900

    [SC-57] feat: Implement geoNames API for Search * Korean supported

---
+++ src/api/geoNameApi.ts
@@ -0,0 +1,16 @@
+import { GeoName } from "@/types/common";
+import { axiosInstance } from "./axios";
+
+const USERNAME = import.meta.env.VITE_API_USERNAME || process.env.GEONAME_USERNAME;
+
+export const geoNameApi = {
+  getGeoName: async (query: string): Promise<GeoName[]> => {
+    const { data } = await axiosInstance.get('/geonames/searchJSON', {
+      params: {
+        name_equals: query,
+        username: USERNAME,
+      }
+    });
+    return data.geonames || [];
+  } 
+}
--- src/api/weatherApi.ts
+++ src/api/weatherApi.ts
@@ -1,14 +1,16 @@
 import { PlacePayload, WeatherInfo } from "@/types/common";
 import { axiosInstance } from "./axios";
 
+const URL = import.meta.env.VITE_API || process.env.API;
+
 export const weatherApi = {
   getWeather: async (url: string, timeUnit: string): Promise<WeatherInfo[]> => {
-    const res = await axiosInstance.get(url);
+    const res = await axiosInstance.get(URL + url);
     return timeUnit === "hourly" ? res.data.hourly.data : [{...res.data.current, icon: res.data.current.icon_num}];
   },
 
   getPlaceID: async (url: string): Promise<PlacePayload> => {
-    const res = await axiosInstance.get(url);
+    const res = await axiosInstance.get(URL + url);
     const place = res?.data[0] ? res.data[0] : res.data;
     return place;
   },
